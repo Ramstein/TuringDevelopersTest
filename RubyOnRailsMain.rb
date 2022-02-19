155086aa9158f2f36a5ce488c038b29a8ac004ff
MD.Zeeshan <gerialworld@gmail.com>
2022-02-19 14:04:53 +0530

    Create RubyOnRailsMain.rb

---
+++ RubyOnRailsMain.rb
@@ -0,0 +1,55 @@
+# Hello World Program in Ruby
+
+# my_b = false || -10 > -9
+# puts my_b
+
+
+# x= int(1011)
+# x= int('1011', 23)
+# x= int(1011, 2)
+# x= int('1011')
+
+
+
+# x = 1
+# if x > 2
+#   puts "x is greater than 2"
+# elsif x <= 2 and x!=0
+#   puts "x is 1"
+# else
+#   puts "I can't guess the number"
+# end
+
+
+
+# my_counter = 1
+# if my_counter <=5
+#     puts my_counter
+#     my_counter = my_counter + 1
+
+
+
+
+# print "What's your first name?"
+# first_name = gets.chomp
+# a = first_name.capitalize
+# a = a.reverse
+# puts "My name is #{a}"
+
+
+
+
+# variable = true
+# if variable
+#     puts "true"
+# else 
+#     puts false
+# end
+
+
+
+my_counter = 2
+while my_counter <=68
+    puts my_counter
+    my_counter **= 2
+end
