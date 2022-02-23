7785695751b92995790ff9ce8a9ff27375e393b3
MD.Zeeshan <gerialworld@gmail.com>
2022-02-24 02:20:54 +0530

    Create javascriptTest.js

---
+++ javascriptTest.js
@@ -0,0 +1,21 @@
+// console.log("Hello, World!");
+
+
+// window.event.returnValue=false;
+
+
+let arr=[1,"t", {x:2}, [3,4]];
+delete arr[1];
+document.write(arr.length);
+
+// let inputs  = [1, "tr", {x:2}, [3,4], {y:5}];
+
+// for(let i=0;i<inputs.length; i++){
+//   if(inputs[i]=="tr") inputs.splice(i, 1);
+//   else console.log(inputs[i]);
+// }
+
+
+// let a = {y:10};
+// a.x = a;
+// console.log(JSON.stringify(a));
